70faeb29459908f6aaa021021b30a794868df785
MihaiMocanuFmi <mihai.mocanu1@stud.ubbcluj.ro>
2023-03-18 23:16:45 +0200

    testing code

--- ImageProcessing.cpp
+++ ImageProcessing.cpp
@@ -7,7 +7,7 @@ int main()
 {
     PortablePixMap_Plain ppm;
 
-    std::string imageStr = "P3           # P3 means this is a RGB color image in ASCII\n"
+    std::string imageStr = "P21           # P3 means this is a RGB color image in ASCII\n"
                            "3 2          # 3 2 is the width and height of the image in pixels\n"
                            "255          # 255 is the maximum value for each color\n"
                            "# The part above is the header\n"
@@ -18,6 +18,29 @@ int main()
                            "255 255   0  # yellow\n"
                            "255 255 255  # white\n"
                            "0   0   0  # black";
+
+    std::cout << "#############################Original################################\n\n"
+              << imageStr << "\n\n"
+              << "#############################Original#############################\n\n";
+
     ppm.loadPlain(imageStr);
+
+    std::string convertedImageStr;
+    ppm.saveToString(convertedImageStr);
+    std::cout << "#############################Conversion1################################\n\n"
+              << convertedImageStr << "\n\n"
+              << "#############################End-Conversion1#############################\n\n";
+
+    PortablePixMap_Plain ppm2;
+    ppm2.loadPlain(convertedImageStr);
+
+    std::string convertedImageStr2;
+    ppm.saveToString(convertedImageStr2);
+
+    std::cout << "#############################Conversion2################################\n\n"
+              << convertedImageStr2 << "\n\n"
+              << "#############################End-Conversion2#############################\n\n";
+
+
     return 0;
 }
