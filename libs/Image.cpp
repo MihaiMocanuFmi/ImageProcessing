1118ceab907bf4422fcf1f898a2da94bedcee74c
MihaiMocanuFmi <mihai.mocanu1@stud.ubbcluj.ro>
2023-03-18 18:24:53 +0200

    Extremely basic structure of Image class

--- libs/Image.cpp
+++ libs/Image.cpp
@@ -1,1 +1,10 @@
 #include "Image.h"
+
+Image::Image(unsigned int width, unsigned int height) : m_size({width, height})
+{
+}
+
+Image::Image(tools::Vector2U size) : m_size(size)
+{
+
+}
--- libs/Image.h
+++ libs/Image.h
@@ -1,12 +1,20 @@
 #ifndef IMAGE_H
 #define IMAGE_H
 
+#include "tools/Vector2.h"
 
 class Image
 {
 private:
+    tools::Vector2U m_size = {};
 
 public:
+    Image() = default;
+
+    Image(unsigned int width, unsigned int height);
+    Image(tools::Vector2U size);
+
+
 };
 
 
