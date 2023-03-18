4787349aeb6833bd9b3298c6e6d24bb1dd6ca586
MihaiMocanuFmi <mihai.mocanu1@stud.ubbcluj.ro>
2023-03-18 23:44:15 +0200

    implementation of image load and save

--- libs/Image.cpp
+++ libs/Image.cpp
@@ -1,5 +1,15 @@
 #include "Image.h"
 
+#include <fstream>
+#include <sstream>
+
+
+///*********************************************************************************************************************
+///*****************************************PUBLIC**********************************************************************
+///*********************************************************************************************************************
+
+
+
 Image::Image(unsigned int width, unsigned int height) : m_size({width, height})
 {
 }
@@ -7,4 +17,32 @@ Image::Image(unsigned int width, unsigned int height) : m_size({width, height})
 Image::Image(tools::Vector2U size) : m_size(size)
 {
 
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+bool Image::load(std::string imagePath)
+{
+    std::ifstream inputFile("imagePath");
+    std::stringstream buffer;
+    buffer << inputFile.rdbuf();
+
+    if (not ppm.loadPlain(buffer.str()))
+        return false;
+
+    inputFile.close();
+    return true;
+}
+bool Image::save(std::string imagePath)
+{
+    std::string imageStr;
+
+    if (not ppm.saveToString(imageStr))
+        return false;
+
+    std::ofstream outputFile("imagePath");
+    outputFile << imageStr;
+
+    outputFile.close();
+    return true;
 }
--- libs/Image.h
+++ libs/Image.h
@@ -1,18 +1,27 @@
 #ifndef IMAGE_H
 #define IMAGE_H
 
+#include <string>
+
+#include "PortablePixMap_Plain.h"
 #include "tools/Vector2.h"
+
 
 class Image
 {
 private:
     tools::Vector2U m_size = {};
 
+    PortablePixMap_Plain ppm = {};
+
 public:
     Image() = default;
 
     Image(unsigned int width, unsigned int height);
     Image(tools::Vector2U size);
+
+    bool load(std::string imagePath);
+    bool save(std::string imagePath);
 
 
 };
