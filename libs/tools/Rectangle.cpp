c5a3714cb99258542f2eb95ee218487e4c5382ab
MihaiMocanuFmi <mihai.mocanu1@stud.ubbcluj.ro>
2023-03-25 15:49:15 +0200

    improved Rectangle class

--- libs/tools/Rectangle.cpp
+++ libs/tools/Rectangle.cpp
@@ -6,4 +6,52 @@
 
 namespace tools
 {
+    Rectangle::Rectangle() : upperLeftCorner{-1, -1}, size{0, 0}
+    {
+
+    }
+
+    Rectangle::Rectangle(Vector2I aUpperLeftCorner, Vector2I aSize) : upperLeftCorner{aUpperLeftCorner}, size{aSize}
+    {
+
+    }
+
+    Rectangle::Rectangle(int x, int y, int width, int height) : upperLeftCorner{x, y}, size{width, height}
+    {
+
+    }
+
+    Vector2I Rectangle::findUpperRight() const
+    {
+        return upperLeftCorner + Vector2I(size.x, 0);
+    }
+
+    Vector2I Rectangle::findLowerLeft() const
+    {
+        return upperLeftCorner + Vector2I(0, size.y);
+    }
+
+    Vector2I Rectangle::findLowerRight() const
+    {
+        return upperLeftCorner + size;
+    }
+
+    bool Rectangle::isContainedInside(const Vector2I &dot, const Rectangle &rectangle)
+    {
+        bool result = rectangle.upperLeftCorner.x <= dot.x;
+        result = result and rectangle.upperLeftCorner.y <= dot.y;
+
+        result = result and dot.x <= rectangle.findLowerRight().x;
+        result = result and dot.y <= rectangle.findLowerRight().y;
+
+        return result;
+    }
+
+    bool Rectangle::isContainedInside(const Rectangle &inner, const Rectangle &outer)
+    {
+        bool result = isContainedInside(inner.upperLeftCorner, outer);
+        result = result and isContainedInside(inner.findLowerRight(), outer);
+
+        return result;
+    }
 } // tools
--- libs/tools/Rectangle.h
+++ libs/tools/Rectangle.h
@@ -11,6 +11,18 @@ namespace tools
     public:
         Vector2I upperLeftCorner;
         Vector2I size;
+
+        Rectangle();
+        Rectangle(Vector2I aUpperLeftCorner,  Vector2I aSize);
+        Rectangle(int x, int y, int width, int height);
+
+
+        Vector2I findUpperRight() const;
+        Vector2I findLowerLeft() const;
+        Vector2I findLowerRight() const;
+
+        static bool isContainedInside(const Vector2I &dot, const Rectangle &rectangle);
+        static bool isContainedInside(const Rectangle &inner, const Rectangle &outer);
     };
 
 } // tools
