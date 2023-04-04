cf9f0ae6b75f5b58d0399760dbfa384576c60cb0
MihaiMocanuFmi <mihai.mocanu1@stud.ubbcluj.ro>
2023-04-05 00:03:14 +0300

    documentation for draw

--- Libs/Draw/Draw.h
+++ Libs/Draw/Draw.h
@@ -8,8 +8,24 @@
 
 namespace Draw
 {
+    /// \brief It draws a Circle on the given image
+    /// \param img The canvas on which the Circle is drawn
+    /// \param center The center of the Circle
+    /// \param radius The radius of the Circle
+    /// \param color The RgbColor of the Circle
     void drawCircle(Image& img, const tools::Vector2I &center, int radius, const tools::RgbColor &color);
+
+    /// \brief It draws a straight Line on the given image
+    /// \param img The canvas on which the Line is drawn
+    /// \param p1 One point of the Line
+    /// \param p2 Another point of the Line
+    /// \param color The RgbColor of the Line
     void drawLine(Image &img, const tools::Vector2I &p1, tools::Vector2I p2, const tools::RgbColor &color);
+
+    /// \brief It draws a Rectangle on the given image
+    /// \param img The canvas on which the Rectangle is drawn
+    /// \param r The Rectangle to be drawn
+    /// \param color The RgbColor of the Rectangle
     void drawRectangle(Image &img, const tools::Rectangle &r, const tools::RgbColor &color);
 
 } // Draw
