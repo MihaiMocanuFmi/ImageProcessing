8cd77332f5450591b71a55a495fc9cd7965f05d6
MihaiMocanuFmi <mihai.mocanu1@stud.ubbcluj.ro>
2023-04-04 21:47:48 +0300

    Comments for Vector2

--- Libs/Tools/Point.h
+++ Libs/Tools/Point.h
@@ -11,8 +11,18 @@ namespace tools
     private:
 
     public:
+        /// \brief Default constructor
+        ///
+        ///     Initializes a 2D point with coords (0, 0)
         Point() = default;
+
+        /// \brief Initializes a 2D point given a Vector2I
+        /// \param pos Position of point
         Point(Vector2I pos);
+
+        /// \brief Initializes a 2D point given its coords
+        /// \param x Horizontal coord axis
+        /// \param y Vertical coord axis
         Point(int x, int y);
     };
 
--- Libs/Tools/Vector2.h
+++ Libs/Tools/Vector2.h
@@ -17,12 +17,19 @@ namespace tools
         T x = 0;
         T y = 0;
 
+        /// \brief Default constructor, initialises coords to (0,0);
         constexpr Vector2() = default;
 
+        /// \brief Initialises given its coords
+        /// \param X Horizontal coord axis
+        /// \param Y Vertical coord axis
         constexpr Vector2(T X, T Y) : x{X}, y{Y}
         {
         }
 
+        /// \brief Initialises Vector2 given another Vector2
+        /// \tparam U Type of given Vector2
+        /// \param vector The Vector2 whose values will be (casted and) copied;
         template<typename U>
         constexpr explicit Vector2(const Vector2<U>& vector): x{static_cast<T>(vector.x)}, y{static_cast<T>(vector.y)}
         {
@@ -46,6 +53,16 @@ namespace tools
     }
 
 
+    /// \relates Vector2
+    /// \brief Overload of binary operator +=
+    ///
+    /// This operator performs a memberwise addition of both vectors,
+    /// and assigns the result to \c left.
+    ///
+    /// \param left  Left operand (a vector)
+    /// \param right Right operand (a vector)
+    ///
+    /// \return Reference to \c left
     template <typename T>
     constexpr Vector2<T>& operator+=(Vector2<T>& left, const Vector2<T>& right)
     {
@@ -55,6 +72,16 @@ namespace tools
         return left;
     }
 
+    /// \relates Vector2
+    /// \brief Overload of binary operator -=
+    ///
+    /// This operator performs a memberwise subtraction of both vectors,
+    /// and assigns the result to \c left.
+    ///
+    /// \param left  Left operand (a vector)
+    /// \param right Right operand (a vector)
+    ///
+    /// \return Reference to \c left
     template <typename T>
     constexpr Vector2<T>& operator-=(Vector2<T>& left, const Vector2<T>& right)
     {
@@ -64,7 +91,13 @@ namespace tools
         return left;
     }
 
-
+    /// \relates Vector2
+    /// \brief Overload of binary operator +
+    ///
+    /// \param left  Left operand (a vector)
+    /// \param right Right operand (a vector)
+    ///
+    /// \return Memberwise addition of both vectors
     template <typename T>
     constexpr Vector2<T> operator+(const Vector2<T>& left, const Vector2<T>& right)
     {
@@ -72,33 +105,67 @@ namespace tools
     }
 
 
+    /// \relates Vector2
+    /// \brief Overload of unary operator -
+    ///
+    /// \param right Vector to negate
+    ///
+    /// \return Memberwise opposite of the vector
     template<typename T>
     constexpr Vector2 <T> operator-(const Vector2<T> &right)
     {
         return Vector2<T>(-right.x, -right.y);
     }
 
+    /// \relates Vector2
+    /// \brief Overload of binary operator -
+    ///
+    /// \param left  Left operand (a vector)
+    /// \param right Right operand (a vector)
+    ///
+    /// \return Memberwise subtraction of both vectors
     template <typename T>
     constexpr Vector2<T> operator-(const Vector2<T>& left, const Vector2<T>& right)
     {
         return Vector2<T>(left.x - right.x, left.y - right.y);
     }
 
-
+    /// \relates Vector2
+    /// \brief Overload of binary operator *
+    ///
+    /// \param left  Left operand (a vector)
+    /// \param right Right operand (a scalar value)
+    ///
+    /// \return Memberwise multiplication by \c right
     template <typename T>
     constexpr Vector2<T> operator*(const Vector2<T>& left, T right)
     {
         return Vector2<T>(left.x * right, left.y * right);
     }
 
-
+    /// \relates Vector2
+    /// \brief Overload of binary operator *
+    ///
+    /// \param left  Left operand (a scalar value)
+    /// \param right Right operand (a vector)
+    ///
+    /// \return Memberwise multiplication by \c left
     template <typename T>
     constexpr Vector2<T> operator*(T left, const Vector2<T>& right)
     {
         return Vector2<T>(right.x * left, right.y * left);
     }
 
-
+    /// \relates Vector2
+    /// \brief Overload of binary operator *=
+    ///
+    /// This operator performs a memberwise multiplication by \c right,
+    /// and assigns the result to \c left.
+    ///
+    /// \param left  Left operand (a vector)
+    /// \param right Right operand (a scalar value)
+    ///
+    /// \return Reference to \c left
     template <typename T>
     constexpr Vector2<T>& operator*=(Vector2<T>& left, T right)
     {
@@ -108,14 +175,29 @@ namespace tools
         return left;
     }
 
-
+    /// \relates Vector2
+    /// \brief Overload of binary operator /
+    ///
+    /// \param left  Left operand (a vector)
+    /// \param right Right operand (a scalar value)
+    ///
+    /// \return Memberwise division by \c right
     template <typename T>
     constexpr Vector2<T> operator/(const Vector2<T>& left, T right)
     {
         return Vector2<T>(left.x / right, left.y / right);
     }
 
-
+    /// \relates Vector2
+    /// \brief Overload of binary operator /=
+    ///
+    /// This operator performs a memberwise division by \c right,
+    /// and assigns the result to \c left.
+    ///
+    /// \param left  Left operand (a vector)
+    /// \param right Right operand (a scalar value)
+    ///
+    /// \return Reference to \c left
     template <typename T>
     constexpr Vector2<T>& operator/=(Vector2<T>& left, T right)
     {
@@ -125,14 +207,30 @@ namespace tools
         return left;
     }
 
-
+    /// \relates Vector2
+    /// \brief Overload of binary operator ==
+    ///
+    /// This operator compares strict equality between two vectors.
+    ///
+    /// \param left  Left operand (a vector)
+    /// \param right Right operand (a vector)
+    ///
+    /// \return True if \c left is equal to \c right
     template <typename T>
     constexpr bool operator==(const Vector2<T>& left, const Vector2<T>& right)
     {
         return (left.x == right.x) and (left.y == right.y);
     }
 
-
+    /// \relates Vector2
+    /// \brief Overload of binary operator !=
+    ///
+    /// This operator compares strict difference between two vectors.
+    ///
+    /// \param left  Left operand (a vector)
+    /// \param right Right operand (a vector)
+    ///
+    /// \return True if \c left is not equal to \c right
     template <typename T>
     constexpr bool operator!=(const Vector2<T>& left, const Vector2<T>& right)
     {
